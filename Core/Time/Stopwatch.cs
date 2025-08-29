a29b23c4f215e16748ab49b6de9c5008f07b1319
hczaja <hubert_czaja@o2.pl>
2025-08-29 17:01:51 +0200

    Add stopwatch and timer

---
+++ Core/Time/Stopwatch.cs
@@ -0,0 +1,24 @@
+﻿using SFML.System;
+
+namespace Core.Time;
+
+internal class Stopwatch : Clock
+{
+    private float InitialTime { get; set; }
+    private float RealTime { get; set; }
+
+    public Stopwatch(float initialTime)
+        => InitialTime = initialTime;
+
+    public bool Update()
+    {
+        RealTime = InitialTime - ElapsedTime.AsSeconds();
+        return RealTime > 0f;
+    }
+
+    public new void Restart()
+    {
+        base.Restart();
+        RealTime = InitialTime;
+    }
+}
---
+++ Core/Time/Timer.cs
@@ -0,0 +1,34 @@
+﻿using SFML.System;
+using System.Drawing;
+
+namespace Core.Time;
+
+public class Timer : Clock
+{
+    private float RealTime { get; set; }
+    private bool IsRunning { get; set; }
+
+    public Timer()
+    {
+        Start();
+    }
+
+    public float Elapsed() => RealTime;
+
+    public void Update()
+    {
+        if (IsRunning)
+            RealTime = ElapsedTime.AsSeconds();
+    }
+
+    public void Stop() => IsRunning = false;
+
+    public void Start() => IsRunning = true;
+
+    public new void Restart()
+    {
+        base.Restart();
+        RealTime = 0f;
+    }
+
+}
